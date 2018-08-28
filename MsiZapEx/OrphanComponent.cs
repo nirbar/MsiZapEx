1fdecdf7b6c9ed35e6a75fcd935f3d57246057b5
nir.bar <nir.bar@panel-sw.com>
2018-08-28 13:35:15 +0300

    Detect orphan components.

---
+++ MsiZapEx/OrphanComponent.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MsiZapEx
+{
+    [Serializable]
+    public class OrphanComponent
+    {
+        public OrphanComponent()
+        {
+            NonExistingClients = new List<string>();
+        }
+
+        public OrphanComponent(string obfusctaedId) : this()
+        {
+            Guid = obfusctaedId.MsiObfuscate();
+        }
+
+        public Guid Guid { get; set; }
+
+        public List<string> NonExistingClients { get; }
+
+        public static List<OrphanComponent> DetectOrphanComponents()
+        {
+            List<OrphanComponent> orphans = new List<OrphanComponent>();
+
+            using (RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (RegistryKey k = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Components", false))
+                {
+                    if (k == null)
+                    {
+                        Console.WriteLine(@"Registry key doesn't exist: SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Components");
+                        return null;
+                    }
+
+                    string[] componentCodes = k.GetSubKeyNames();
+                    foreach (string c in componentCodes)
+                    {
+                        // Ignore default value
+                        if (string.IsNullOrWhiteSpace(c) || c.Equals("@"))
+                        {
+                            continue;
+                        }
+
+                        using (RegistryKey k2 = k.OpenSubKey(c))
+                        {
+                            OrphanComponent orphan = OrphanComponent.IsOrphanComponent(k2);
+                            if (orphan != null)
+                            {
+                                orphans.Add(orphan);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return orphans;
+        }
+
+        public static OrphanComponent IsOrphanComponent(RegistryKey componentKey)
+        {
+            string[] productCodes = componentKey.GetValueNames();
+
+            string compName = Path.GetFileName(componentKey.Name);
+            OrphanComponent orphan = new OrphanComponent(compName);
+
+            foreach (string p in productCodes)
+            {
+                // Ignore default value
+                if (string.IsNullOrWhiteSpace(p) || p.Equals("@") || p.Equals("00000000000000000000000000000000"))
+                {
+                    continue;
+                }
+
+                Guid productCode = p.MsiObfuscate();
+                Guid componentGuid = compName.MsiObfuscate();
+
+                IEnumerable<ProductInstallation> pis = ProductInstallation.GetProducts(productCode.ToString("B"), null, UserContexts.All);
+                if ((pis == null) || (pis.Count() <= 0))
+                {
+                    string keyPath = componentKey.GetValue(p) as string;
+                    Console.WriteLine($"Component '{componentGuid.ToString("B")}' in registry key '{componentKey.Name}' is registered to product '{productCode.ToString("B")} which is not installed with KeyPath='{keyPath}'");
+                    orphan.NonExistingClients.Add(productCode.ToString("B"));
+                }
+            }
+
+            return orphan.NonExistingClients.Count > 0 ? orphan : null;
+        }
+    }
+}
--- MsiZapEx/Program.cs
+++ MsiZapEx/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using Microsoft.Deployment.WindowsInstaller;
+using System.IO;
 
 namespace MsiZapEx
 {
@@ -10,14 +13,7 @@ namespace MsiZapEx
     {
         static void Main(string[] args)
         {
-            Guid g = new Guid("c301f105-d0a2-4ba9-9de4-498eea078900");
-            Console.WriteLine($"From {g.ToString("B")} To {g.MsiObfuscate()} And back to {g.MsiObfuscate().MsiObfuscate().ToString("B")}");
-
-            g = new Guid("B19ED5D0-2262-43F7-AA89-48B43CDEA161");
-            Console.WriteLine($"From {g.ToString("B")} To {g.MsiObfuscate()} And back to {g.MsiObfuscate().MsiObfuscate().ToString("B")}");
-
-            g = new Guid("8EEF8D7F-0EFF-5DB7-BEA6-BC2E776A4AAE");
-            Console.WriteLine($"From {g.ToString("B")} To {g.MsiObfuscate()} And back to {g.MsiObfuscate().MsiObfuscate().ToString("B")}");
+            OrphanComponent.DetectOrphanComponents();
         }
     }
 }
