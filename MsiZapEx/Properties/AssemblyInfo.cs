ebcc196977869bb7434365064d7008e18bf5542c
Nir Bar <nir.bar@panel-sw.co.il>
2023-04-30 07:39:05 +0300

    Initial- net6 build

--- MsiZapEx/Properties/AssemblyInfo.cs
+++ MsiZapEx/Properties/AssemblyInfo.cs
@@ -1,14 +1,9 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MsiZapEx")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MsiZapEx")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
